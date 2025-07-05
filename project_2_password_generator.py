850934aeb14c6cae213680544b0062a8826ed2c0
Shashwat9722 <shashwatv495@gmail.com>
2025-07-05 21:38:11 +0530

    Update project_2_password_generator.py

--- project_2_password_generator.py
+++ project_2_password_generator.py
@@ -5,14 +5,12 @@ def generate_password(length=12):
     if length < 4:
         print("Password length should be at least 4.")
         return ""
-    # Ensure the password has at least one lowercase, uppercase, digit, and symbol
     chars = [
         random.choice(string.ascii_lowercase),
         random.choice(string.ascii_uppercase),
         random.choice(string.digits),
         random.choice(string.punctuation)
     ]
-    # Fill the rest of the password length with random choices
     chars += random.choices(
         string.ascii_letters + string.digits + string.punctuation,
         k=length-4
